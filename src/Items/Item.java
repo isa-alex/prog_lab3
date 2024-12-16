75fba4346b3019c176237c5a04834fcb746dbf0a
Alex <124351863+isa-alex@users.noreply.github.com>
2024-12-16 05:50:37 +0300

    Update Item.java

--- src/Items/Item.java
+++ src/Items/Item.java
@@ -8,40 +8,4 @@ public abstract class Item {
     public It_info getItInfo() {
         return info;
     }
-/*
-    private Item_type curr_type = Item_type.NONE;
-
-    public Item(String it_name) {
-        this.it_name = it_name;
-    }
-
-    public String getName() {
-        return it_name;
-    }
-
-    public void setName(String it_name) {
-        this.it_name = it_name;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return it_name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return Objects.equals(it_name, item.it_name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(it_name);
-    }
-
- */
 }
